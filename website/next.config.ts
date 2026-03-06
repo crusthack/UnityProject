28049c5aa6fba274b455a26cf475e19546daec06
crusthack <crusthack@gmail.com>
2026-03-06 23:59:11 +0900

    config 수정

--- website/next.config.ts
+++ website/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+// website/next.config.ts
+const nextConfig = {
+  output: 'export', // 이 줄이 있어야 website/out 폴더가 생깁니다!
+  distDir: 'out',   // (선택사항) 빌드 결과물 이름을 명시적으로 지정
 };
 
 export default nextConfig;
