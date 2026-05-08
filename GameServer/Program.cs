807d2b19ac927a5c2eaa570dcd5e98a0e5018432
crusthack <crusthack@gmail.com>
2026-05-08 22:31:38 +0900

    로컬에서 직접 쉘 접속통한건 잘 됨

--- GameServer/Program.cs
+++ GameServer/Program.cs
@@ -11,7 +11,7 @@ namespace GameServer
             var serverName = builder.Configuration["Custom:ServerName"];
             var serverCapa = builder.Configuration["Custom:ServerCapacity"];
             Console.WriteLine($"Server Name: {serverName}");
-            Console.WriteLine($"Server Capa: {serverCapa}");
+            Console.WriteLine($"Server Capacity: {serverCapa}");
             Console.WriteLine($"Login Server Address: {builder.Configuration["Custom:LoginServerURL"]}");
 
             GameServerContext.Init(builder.Configuration);  
